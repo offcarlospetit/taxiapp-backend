ae5299949f31e6169475634b4030b7ae8da442f6
Carlos Petit <ca.alberto.p@gmail.com>
2020-09-09 16:36:52 -0300

    update server

--- build/index.js
+++ build/index.js
@@ -2,14 +2,26 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+let driverSocket;
+let passengerSocket;
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-    socket.on("chat Message", (message) => {
-        console.log(message);
+    socket.on("driverRequest", (route) => {
+        console.log("driverRequest from passenger");
+        passengerSocket = socket;
+        if (driverSocket)
+            driverSocket.emit("driverRequest", route);
     });
-    console.log('a user connected');
+    socket.on("driverLocation", (location) => {
+        console.log("driverLocation from driver");
+        passengerSocket.emit('driverLocation', location);
+    });
+    socket.on("lookingPassanger", () => {
+        console.log("lookingPassanger from driver");
+        driverSocket = socket;
+    });
 });
 http.listen(3009, () => {
     console.log('listening on *:3009');
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,31 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let driverSocket: SocketIO.Socket;
+let passengerSocket: SocketIO.Socket;
+
+
 app.get('/', (req: any, res: any) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket: any) => {
-    socket.on("chat Message", (message: string) => {
-        console.log(message)
+io.on('connection', (socket: SocketIO.Socket) => {
+    socket.on("driverRequest", (route: any) => {
+        console.log("driverRequest from passenger")
+        passengerSocket = socket
+        if (driverSocket) driverSocket.emit("driverRequest", route)
     })
-    console.log('a user connected');
+
+    socket.on("driverLocation", (location: { latitude: any, longitude: any }) => {
+        console.log("driverLocation from driver")
+        passengerSocket.emit('driverLocation', location)
+    })
+
+    socket.on("lookingPassanger", () => {
+        console.log("lookingPassanger from driver")
+        driverSocket = socket
+    })
+
 });
 
 http.listen(3009, () => {
